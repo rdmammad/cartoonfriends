459432570756770a6b1fc065445abb4096746abd
Raidə Məmmədova <raida.mammadova@nspsolutions.net>
2020-01-18 16:50:38 +0400

    last ches, cartoonfriend done

--- src/App.js
+++ src/App.js
@@ -3,36 +3,45 @@ import {CardList} from "./components/card-list/card-list.component";
 import {SearchBox} from "./components/search-box/search-box.component";
 import './App.css';
 
-class App extends Component{
-  constructor(){
-    super();
+class App extends Component {
+    constructor() {
+        super();
 
-    this.state = {
-      cartoons : [],
-      searchField : '',
+        this.state = {
+            cartoons: [],
+            searchField: '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(resp => resp.json())
+            .then(users => this.setState({cartoons: users}));
+    }
+
+    handleChange(e) {
+        if (e.target.value != "") {
+            this.setState({searchField: e.target.value})
+        }
     };
-  }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(resp => resp.json())
-        .then(users => this.setState({cartoons : users}));
-  }
-
-  render() {
-    const {cartoons, searchField} = this.state;
-    const filteredCartoons = cartoons.filter(cartoon =>
-        cartoon.name.toLowerCase().includes(searchField.toLowerCase())
-    );
-    return (
-        <div className="App">
-          <SearchBox
-              placeholder="Search for cartoon"
-              handleChange={e => this.setState({searchField : e.target.value})}/>
-          <CardList cartoons={filteredCartoons} />
-        </div>
-    );
-  }
+    render() {
+        const {cartoons, searchField} = this.state;
+        const filteredCartoons = cartoons.filter(cartoon =>
+            cartoon.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+        return (
+            <div className="App">
+                <h1>Cartoon characters</h1>
+                <SearchBox
+                    placeholder="Search for cartoon"
+                    handleChange={this.handleChange}/>
+                <CardList cartoons={filteredCartoons}/>
+            </div>
+        );
+    }
 }
 
 export default App;
